92fe80c6f4fa8fb473d8913759aff2f0555e2dc3
bimenegazzo <75815470+bimenegazzo@users.noreply.github.com>
2020-12-10 21:54:44 -0500

    Add files via upload

---
+++ data/covidEvents.js
@@ -0,0 +1,40 @@
+var covidEvents = [{
+    label: "Stay-At-Home Order",
+    x: new Date("3/22/2020"),
+    place: [
+        {x: (new Date(2020, 2, 22)), y: 0},
+        {x: (new Date(2020, 2, 22)), y: 1}
+    ]
+},
+{
+    label: "Phase 1 Reopening",
+    x: new Date("6/8/2020"),
+    place: [
+        {x: (new Date(2020, 5, 8)), y: 0},
+        {x: (new Date(2020, 5, 8)), y: 1}
+    ]
+},
+{
+    label: "Phase 2 Reopening",
+    x: new Date("6/22/2020"),
+    place: [
+        {x: (new Date(2020, 5, 22)), y: 0},
+        {x: (new Date(2020, 5, 22)), y: 1}
+    ]
+},
+{
+    label: "Phase 3 Reopening",
+    x: new Date("7/6/2020"),
+    place: [
+        {x: (new Date(2020, 6, 6)), y: 0},
+        {x: (new Date(2020, 6, 6)), y: 1}
+    ]
+},
+{
+    label: "Phase 4 Reopening",
+    x: new Date("7/19/2020"),
+    place: [
+        {x: (new Date(2020, 6, 19)), y: 0},
+        {x: (new Date(2020, 6, 19)), y: 1}
+    ]
+}]
---
+++ data/publicHolidays.js
@@ -0,0 +1,161 @@
+var publicHolidays = [{
+    label: "Memorial Day",
+    x: new Date("5/25/2020"),
+    place: [
+        {x: (new Date("5/25/2020")), y: 0},
+        {x: (new Date("5/25/2020")), y: 1}
+    ]
+},
+{
+    label: "Independence Day",
+    x: new Date("7/4/2020"),
+    place: [
+        {x: (new Date("7/4/2020")), y: 0},
+        {x: (new Date("7/4/2020")), y: 1}
+    ]
+},
+{
+    label: "Labor Day",
+    x: new Date("9/7/2020"),
+    place: [
+        {x: (new Date("9/7/2020")), y: 0},
+        {x: (new Date("9/7/2020")), y: 1}
+    ]
+},
+{
+    label: "Indigenous Peoples' Day",
+    x: new Date("10/12/2020"),
+    place: [
+        {x: (new Date("10/12/2020")), y: 0},
+        {x: (new Date("10/12/2020")), y: 1}
+    ]
+},
+{
+    label: "Halloween",
+    x: new Date("10/31/2020"),
+    place: [
+        {x: (new Date("10/31/2020")), y: 0},
+        {x: (new Date("10/31/2020")), y: 1}
+    ]
+},
+{
+    label: "Veterans Day",
+    x: new Date("11/11/2020"),
+    place: [
+        {x: (new Date("11/11/2020")), y: 0},
+        {x: (new Date("11/11/2020")), y: 1}
+    ]
+},
+{
+    label: "Thanksgiving",
+    x: new Date("11/26/2020"),
+    place: [
+        {x: (new Date("11/26/2020")), y: 0},
+        {x: (new Date("11/26/2020")), y: 1}
+    ]
+},
+{
+    label: "Christmas Day",
+    x: new Date("12/25/2020"),
+    place: [
+        {x: (new Date("12/25/2020")), y: 0},
+        {x: (new Date("12/25/2020")), y: 1}
+    ]
+},
+{
+    label: "New Year's Day",
+    x: new Date("1/1/2021"),
+    place: [
+        {x: (new Date("1/1/2021")), y: 0},
+        {x: (new Date("1/1/2021")), y: 1}
+    ]
+},
+{
+    label: "MLK Day",
+    x: new Date("1/18/2021"),
+    place: [
+        {x: (new Date("1/18/2021")), y: 0},
+        {x: (new Date("1/18/2021")), y: 1}
+    ]
+},
+{
+    label: "Lincoln's Birthday",
+    x: new Date("2/12/2021"),
+    place: [
+        {x: (new Date("2/12/2021")), y: 0},
+        {x: (new Date("2/12/2021")), y: 1}
+    ]
+},
+{
+    label: "Washington's Birthday",
+    x: new Date("2/15/2021"),
+    place: [
+        {x: (new Date("2/15/2021")), y: 0},
+        {x: (new Date("2/15/2021")), y: 1}
+    ]
+},
+{
+    label: "Memorial Day",
+    x: new Date("5/31/2021"),
+    place: [
+        {x: (new Date("5/31/2021")), y: 0},
+        {x: (new Date("5/31/2021")), y: 1}
+    ]
+},
+{
+    label: "Independence Day",
+    x: new Date("7/4/2021"),
+    place: [
+        {x: (new Date("7/4/2021")), y: 0},
+        {x: (new Date("7/4/2021")), y: 1}
+    ]
+},
+{
+    label: "Labor Day",
+    x: new Date("9/6/2021"),
+    place: [
+        {x: (new Date("9/6/2021")), y: 0},
+        {x: (new Date("9/6/2021")), y: 1}
+    ]
+},
+{
+    label: "Indigenous Peoples' Day",
+    x: new Date("10/11/2021"),
+    place: [
+        {x: (new Date("10/11/2021")), y: 0},
+        {x: (new Date("10/11/2021")), y: 1}
+    ]
+},
+{
+    label: "Halloween",
+    x: new Date("10/31/2021"),
+    place: [
+        {x: (new Date("10/31/2021")), y: 0},
+        {x: (new Date("10/31/2021")), y: 1}
+    ]
+},
+{
+    label: "Veterans Day",
+    x: new Date("11/11/2021"),
+    place: [
+        {x: (new Date("11/11/2021")), y: 0},
+        {x: (new Date("11/11/2021")), y: 1}
+    ]
+},
+{
+    label: "Thanksgiving",
+    x: new Date("11/25/2021"),
+    place: [
+        {x: (new Date("11/25/2021")), y: 0},
+        {x: (new Date("11/25/2021")), y: 1}
+    ]
+},
+{
+    label: "Christmas Day",
+    x: new Date("12/25/2021"),
+    place: [
+        {x: (new Date("12/25/2021")), y: 0},
+        {x: (new Date("12/25/2021")), y: 1}
+    ]
+}
+]
